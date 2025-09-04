511e214da6e06f709548b3649783702f924e1f47
Johannes Kamprad <archlinux@kamprad.net>
2025-09-04 12:28:23 +0200

    Update openweather.sh

--- weather/openweather.sh
+++ weather/openweather.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 #
+# screenshot_all - Screenshot helper for Sway
+#
+# Copyright (C) 2025 Johannes Kamprad
+#
+# This script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this script.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Author: Johannes Kamprad (joekamprad)
+#
+
 # OpenWeatherMap Weather Fetcher for Waybar/Status Bars
 # Secure API key handling with configurable locations
-#
+# config: $CONFIG_DIR/openweather/config
+# use `-s` option on the script for setup
 
 set -euo pipefail
 
